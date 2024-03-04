a9f86f10ed0315e2e150c5c0a005c21839484427
THIAGO CORREA BRANDAO <202301135265@alunos.ibmec.edu.br>
2024-03-04 11:35:23 -0300

    aulaa..

--- AC4/main.go
+++ AC4/main.go
@@ -1,7 +1,43 @@
-algoritimo_Hanoi(n int , A ){
+package ac4
+
+import()
+
+func algoritimo_Hanoi(n int , A , B , C rune){
 
 }
 
+func buscaMatriz(m [][]int, n , k int)bool{
+	i:= 0
+	var j int 
+	for i < n {
+		j = 0
+		for j < n{
+			if m[i][j] == k{
+				return true
+			}
+			j ++
+		}
+		i++
+	}
+	return false
+}
+
+func somaPar(a array, tamanho int,alvo int){
+	i:= 0
+	for i < tamanho - 1{
+		j:= i + 1
+		for j <  tamanho{
+			if a[i] + a[j] = alvo{
+				return a[i], a[j]
+			}
+			j++
+		}
+		i++
+	}
+	return (-1,-1)
+}
+
 func main(){
-	
+	matriz := [][]int := {[1,2],[2,4]}
+	buscaMatriz([][],4,2)
 }
