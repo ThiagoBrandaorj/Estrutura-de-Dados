49fa75b08508777bd460b2033271e36a0a602d7a
Thiago <thiagocbrandao2003@gmail.com>
2024-03-06 20:56:13 -0300

    AC4 finalizada

--- AC4/main.go
+++ AC4/main.go
@@ -3,69 +3,32 @@ package main
 import("fmt")
 
 // 1
-func torreHanoi(n int , A[]int , B[]int , C[]int) int{
-	movimentos := make([]int,0)
-	j:= 0
-	if n > 0 && A == nil{
-		for i:= 0 ;i<n; i++{
-			A[i] = n - i
-		}
-		return movimentos[j] + torreHanoi(n - 1, A , C , B)
-	}else if n == 2{
-		for i:= 0 ; i < 2; i++{
-			A[i] = 
-			movimentos[j] ++
-		}  
-	}else if n == 0{
-		return 0
-	} else{
-		fmt.Println("não deveria chegar aqui")
+func torreHanoi(n int , A , B , C string){
+	if n > 0{
+		torreHanoi(n - 1 , A , C , B)
+		fmt.Println("Mover o disco do topo de " , A ," para " , B)
+		torreHanoi(n - 1 , C , B , A)
 	}
 }
 
 // 2
-func aleatoria(a []int , valor int){
-	var soma int
+func encontraPar(a []int , valor int) (int,int){
 	for i:= 0 ; i < len(a); i++{
-		for j:= 0 ; j < len(a) - 1 ; j++
+		for j:= 1 ; j < len(a) ; j++{
+			if a[i] + a[j] == valor{
+				return a[i] , a[j]
+			}
+		}
 	}
-}
-
-func fatorial(n int) int{
-	if n > 0{
-		return n * fatorial(n - 1)
-	}else if n == 0{
-		return 1
-	}else{
-		fmt.Println("Digite um numero inteiro")
-		return -1
-	}
+	return -1,-1
 }
 
 func main() {
-	var x,n, tam int
-	var a []int
-	var b []int
-	var c []int
-	fmt.Println("Digite um número inteiro:")
-	_, err := fmt.Scanln(&x)
-
-	if err != nil {
-		fmt.Println("Erro ao ler o número:", err)
-		return
-	}
-
-	result := fatorial(x)
-	if result != -1 {
-		fmt.Println("Fatorial de", x, "é:", result)
-	}
-
-	fmt.Println("Digite o número de peças para o algoritimo de hanoi: ")
-	fmt.Scanln(&n)
-	torreHanoi(n,a,b,c)
-	
-	fmt.Println("Digite o tamanho do array ordenado que deseja: ")
-	fmt.Scanln(&tam)
-	array := [tam] int{for i:=0;i<}
-	for i:= 0 
+	A , B , C := "A" , "B" , "C"
+	n := 3
+	torreHanoi(n , A , B , C)
+	array := []int{1,2,4,5,8,11,12}
+	alvo := 16
+	n1 , n2 := encontraPar(array, alvo)
+	fmt.Println(n1 , n2)
 }
