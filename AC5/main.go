332be55d72c7a0ac0eb0087f17950625730de0c8
Thiago <thiagocbrandao2003@gmail.com>
2024-03-16 23:57:05 -0300

    AC5 feita

--- AC5/main.go
+++ AC5/main.go
@@ -1,36 +1,77 @@
 package main
 
-import()
+import (
+	"fmt"
+	"math"
+)
 
-func BuscaBin (L [16]int , n int ,  x int) int{
-	in := 0
-	fim := n - 1
-	for in <= fim {
-		meio := (in + fim) % 2
-		if L[meio] == x{
-			return meio
-		}else{
-			if L[meio] < x{
-				in = meio + 1
-			}else{
-				fim = meio - 1
-			}		
+func Triangulo(){
+	var a,b,c float64
+	array := [3] *float64{&a,&b,&c}
+	var mensagem = ""
+	fmt.Scan(&a,&b,&c)
+	for i:= 0; i < len(array) - 1; i++{
+		for j:= 0; j < len(array) - i - 1; j++{
+			if *array[j] < *array[j + 1]{
+				var temp = *array[j]
+				array[j] = array[j+1]
+				array[j+1] = &temp
+			}
 		}
 	}
-	return -1
+	if *array[0] >= *array[1] + *array[2]{
+		mensagem += "NAO FORMA TRIANGULO \n"
+	}else if math.Pow(*array[0],2) == math.Pow(*array[1],2) + math.Pow(*array[2],2){
+		mensagem += "TRIANGULO RETANGULO \n"
+	} else if math.Pow(*array[0],2) > math.Pow(*array[1],2) + math.Pow(*array[2],2){
+		mensagem += "TRIANGULO OBTUSANGULO \n" 
+	}else if math.Pow(*array[0],2) < math.Pow(*array[1],2) + math.Pow(*array[2],2){
+		mensagem += "TRIANGULO ACUTANGULO \n"
+	}
+
+	if *array[0] == *array[1] && *array[1] == *array[2]{
+		mensagem += "TRIANGULO EQUILATERO \n"
+	} else if *array[0]==*array[1] || *array[0]==*array[2] || *array[1]==*array[2]{
+		mensagem += "TRIANGULO ISOSCELES \n"
+	}
+	fmt.Println(mensagem)
 }
 
-func InserePosicao(L []int , n int , m int , valor int , pos int){
-	if n == 
-	for i:= 0 ;  ; i++{
+func CrescimentoPopulacional() {
+	var T int
+	fmt.Scanln(&T)
 
+	if T >= 1 && T <= 3000 {
+		for i := 0; i < T; i++ {
+			var PA, PB int
+			var G1, G2 float64
+			fmt.Scan(&PA, &PB, &G1, &G2)
+			if PA >= 100 && PA <= 1000000 && PB >= 100 && PB <= 1000000 &&
+				G1 >= 0.1 && G1 <= 10.0 && G2 >= 0.0 && G2 <= 10.0 {
+				tempo := 0
+				for PA <= PB {
+					PA = int(math.Round(float64(PA) * (1 + G1/100)))
+					PB = int(math.Round(float64(PB) * (1 + G2/100)))
+
+					tempo++
+					if tempo > 100 {
+						fmt.Println("Mais de 1 seculo")
+						break
+					}
+				}
+				if tempo <= 100 {
+					fmt.Println(tempo, " Anos")
+				}
+			}
+		}
 	}
 }
 
-func main(){
-	a := [...] int{-10,-5,-3,0,2,6,8,9,12,16,25,30,32,36,38,45}
-	n := len(a)
-	x := 21
-	r := BuscaBin(a , n , x)
-	println(r)
+func main() {
+	// 1
+	fmt.Println("Hello World!")
+	// 2
+	Triangulo()
+	// 3
+	CrescimentoPopulacional()
 }
--- algoritimos/algoritimo.go
+++ algoritimos/algoritimo.go
@@ -46,8 +46,76 @@ func maiorNumero() {
 	fmt.Println(maior)
 }
 
+func CartasOrdem() {
+    var n1, n2 int
+    classificacao := ""
+
+    for {
+        fmt.Scan(&n2)
+
+        if n1 == 0 {
+            n1 = n2
+            continue
+        }
+
+        if n2 > n1 {
+            if classificacao == "" {
+                classificacao = "C"
+            } else if classificacao == "D" {
+                classificacao = "N"
+                break
+            }
+        }
+
+        if n2 < n1 {
+            if classificacao == "" {
+                classificacao = "D"
+            } else if classificacao == "C" {
+                classificacao = "N"
+                break
+            }
+        }
+
+        n1 = n2
+    }
+
+    fmt.Println(classificacao)
+}
+
+func BuscaBin (L [16]int , n int ,  x int) int{
+	in := 0
+	fim := n - 1
+	for in <= fim {
+		meio := (in + fim) % 2
+		if L[meio] == x{
+			return meio
+		}else{
+			if L[meio] < x{
+				in = meio + 1
+			}else{
+				fim = meio - 1
+			}		
+		}
+	}
+	return -1
+}
+
+/*func InserePosicao(L []int , n int , m int , valor int , pos int){
+	if n == 
+	for i:= 0 ;  ; i++{
+
+	}
+}
+*/
+
 func main() {
 	// tomadas()
-	 meteoritos()
+	//meteoritos()
+	CartasOrdem()
 	//maiorNumero()
+	a := [...] int{-10,-5,-3,0,2,6,8,9,12,16,25,30,32,36,38,45}
+	n := len(a)
+	x := 21
+	r := BuscaBin(a , n , x)
+	println(r)
 }
