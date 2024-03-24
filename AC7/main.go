ac79b622daaac01ad155a299d76c0cec43090edc
Thiago <thiagocbrandao2003@gmail.com>
2024-03-24 18:11:50 -0300

    ac7 quase pronta

--- AC7/main.go
+++ AC7/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
-
+import (
+    "fmt"
+)
+func mdc(num, den int) int {
+    for den != 0 {
+        num, den = den, num%den
+    }
+    return num
+}
 // 1
 func tdaracional(){
 	var N int
@@ -31,20 +38,50 @@ func tdaracional(){
 					res_numerador = n1/d1
 					res_denominador = n2/d2
 				}
-				num := &res_numerador
-				den := &res_denominador
-				for *den != 0{
-					*num,*den = *den, *num % *den
-				}
-				mdc := &num
-				res_numerador_simplificado := res_numerador / mdc
-				res_denominador_simplificado := res_denominador / mdc
-				fmt.Print(res_numerador + )
-
-			 }
+				mdc := mdc(res_numerador,res_denominador)
+				res_numerador /= mdc
+                res_denominador /= mdc
+        
+                fmt.Printf("%d/%d = %d/%d\n", res_numerador,res_denominador,res_numerador,res_denominador)
+			}
 		}
 	}
 }
-func main(){
 
+// 2
+func ricoDoMate(){
+    var n int
+	var l, q float32
+
+	fmt.Scanln(&n, &l, &q)
+
+	participantes := make([]string,n)
+	for i := 0; i < n; i++ {
+		fmt.Scan(&participantes[i])
+	}
+
+	i := 0
+	for l - q > 0{
+		l -= q
+		i++
+	}
+	i %= n
+	fmt.Printf("%s %.1f\n", participantes[i], l)
+}
+
+// 3
+func calculoSimples(){
+    var cod1,cod2, n1,n2 int
+    var valor1,valor2 float32
+    fmt.Scanln(&cod1,&n1,&valor1)
+    fmt.Scanln(&cod2,&n2,&valor2)
+    total := (float32(n1) * valor1) + (float32(n2)*valor2)
+    fmt.Printf("VALOR A PAGAR: R$ %.2f", total)
+}
+
+func main() {
+	
+	tdaracional()
+    //ricoDoMate()
+    //calculoSimples()
 }
